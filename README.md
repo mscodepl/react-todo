572e3ec99a51da5b7f08652a01bd9b8eedaa6670
mscodepl <mateusz@mscode.pl>
2024-11-04 19:45:18 +0100

    Improve responsiveness and documentation
    - Adjusted background div dimensions for responsive design in `App.tsx`
    - Updated text and layout styles for better scalability across devices
    - Enhanced README with project logo and tech stack visualization
    - Added MIT License file for open-source distribution
    - Modified `dev` script in `package.json` to allow network access via `--host 0.0.0.0`

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# TODO
+<div align="center">
+    <img src="https://cms.mscode.pl/uploads/react_8845e4defa.png" height="100" alt="Logo" style="border-radius: 12px; margin-top: 4px; margin-bottom: -24px;">
+
+  <h1>Simple React TODO</h1>
+
+![Tech stack](https://skillicons.dev/icons?i=css,html,typescript,tailwind,react,vite&perline=3)
+
+📜 Kod źródłowy jest udostępniany na licencji **MIT**. Więcej szczegółów znajdziesz w pliku [LICENSE](LICENSE).
+</div>
+
+---
--- src/App.tsx
+++ src/App.tsx
@@ -56,22 +56,20 @@ const App: React.FC = () => {
 
     return (
         <div className="text-slate-200 flex flex-col items-center justify-center min-h-screen w-full bg-[#131720] relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://cms.mscode.pl/uploads/hero_058066354c.svg')] before:opacity-10 before:bg-no-repeat before:bg-top before:w-full before:h-full before:z-0 before:transform before:-translate-x-1/2 font-Montserrat">
-            <div className="absolute -left-44 -top-44 w-[550px] h-[350px] opacity-20 bg-[#0082ff] blur-[190px] rounded z-10"></div>
-            <div className="absolute -right-44 bottom-44 w-[450px] h-[450px] opacity-30 bg-[#0082ff] blur-[110px] rounded z-10"></div>
+            <div className="absolute -left-44 -top-44 w-[350px] h-[250px] sm:w-[550px] sm:h-[350px] opacity-20 bg-[#0082ff] blur-[190px] rounded z-10"></div>
+            <div className="absolute -right-44 bottom-44 w-[250px] h-[250px] sm:w-[450px] sm:h-[450px] opacity-30 bg-[#0082ff] blur-[110px] rounded z-10"></div>
 
-            <div className="w-full max-w-xl z-20">
-                <div className="text-center mb-8 font-bold">
-                    <div className="text-center mb-8 fon font-bold">
-                        <span className="from-blue-600 via-sky-500 to-blue-600 animate-gradient-x bg-gradient-to-r bg-clip-text text-transparent block text-4xl lg:text-6xl">
-                            Lista Zadań
-                        </span>
-                        <span className="from-gray-200 via-gray-300 to-gray-200 bg-gradient-to-r bg-clip-text pb-4 -mb-4 text-transparent text-3xl lg:text-4xl">
-                            Zarządzaj swoimi zadaniami efektywnie i w stylu.
-                        </span>
-                    </div>
+            <div className="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl z-20">
+                <div className="text-center mb-6 sm:mb-8 font-bold">
+                    <span className="from-blue-600 via-sky-500 to-blue-600 animate-gradient-x bg-gradient-to-r bg-clip-text text-transparent block text-3xl sm:text-4xl lg:text-6xl">
+                        Lista Zadań
+                    </span>
+                    <span className="from-gray-200 via-gray-300 to-gray-200 bg-gradient-to-r bg-clip-text pb-2 sm:pb-4 -mb-2 sm:-mb-4 text-transparent text-xl sm:text-3xl lg:text-4xl">
+                        Zarządzaj swoimi zadaniami efektywnie i w stylu.
+                    </span>
                 </div>
                 <TodoInput addTodo={addTodo} />
-                <div className="bg-gray-800/50 shadow-xl rounded-2xl px-6 py-8 flex flex-col justify-center">
+                <div className="bg-gray-800/50 shadow-xl rounded-2xl px-4 py-6 sm:px-6 sm:py-8 flex flex-col justify-center">
                     {filteredTodos.length > 0 ? (
                         filteredTodos.map((todo: Todo) => (
                             <TodoItem
@@ -86,10 +84,10 @@ const App: React.FC = () => {
                     ) : (
                         <p className="text-center text-gray-400">Brak zadań do wyświetlenia.</p>
                     )}
-                    <span className="text-gray-400 text-center mt-5">
+                    <span className="text-gray-400 text-center mt-4 sm:mt-5">
                         Pozostało do zrobienia: <b>{remainingTodos}</b>
                     </span>
-                    <div className="flex justify-between items-center mt-2">
+                    <div className="flex flex-col sm:flex-row justify-between items-center mt-3 sm:mt-2 space-y-2 sm:space-y-0">
                         <FilterButtons currentFilter={filter} setFilter={setFilter} />
                         <ClearCompletedButton onClick={clearCompleted} />
                     </div>
